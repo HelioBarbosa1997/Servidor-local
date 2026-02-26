a264e3f48fa820536b08607bc4dbb9129aee6077
Helio Barbosa <barbosahifb1997@gmail.com>
2026-02-26 12:31:35 -0100

    add funcoes de servicos

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,54 @@
-import express from "express"
-import { adicionarServico } from "./servico.js"
+import express, { type Request, type Response } from "express"
+import { adicionarServico, listarServicos, apagarServico, obterServico } from "./servico.js"
 
-const app = express();
+const app = express()
+app.use(express.json())
 
 app.get("/hello", (req, res) => {
     console.log("Hello Word");
     res.send("Hello Word");
 });
 
-app.get("/adicionar-servico", (req, res) => {
+// rota pata adicionar serviço novo
+app.post("/adicionar-servico", (req: Request, res: Response) => {
     const novoServico = req.body
+
+    console.log(novoServico)
+    const addServicoResponse = adicionarServico(novoServico)
+    res.json(addServicoResponse)
+
     adicionarServico(novoServico)
+})
+
+// rota para listar todos os serviços
+app.get("/listar-servico", (req: Request, res: Response) => {
+    const listServicoResponse = listarServicos()
+
+    res.json(listServicoResponse)
+})
+// Rota para apagar servico
+app.delete("/apagar-servico", (req: Request, res: Response) => {
+    const { nome } = req.query
+    if (nome) {
+        const apagarServicoResponse = apagarServico(nome as string)
+        res.json(apagarServicoResponse)
+    } else {
+        res.json({
+            menssage: "Nome do servico e obrigatorio"
+        })
+    }
+})
+//rota para obter serviço pelo nome
+app.get("/obter-servico", (req: Request, res: Response) => {
+    const { nome } = req.query
+    if (nome) {
+        const obterServicoResponse = obterServico(nome as string)
+        res.json(obterServicoResponse)
+    } else {
+        res.json({
+            message: "Nome do servico e obrigatorio"
+        })
+    }
 })
 
 app.listen(8080, () => {
--- src/servico.ts
+++ src/servico.ts
@@ -8,6 +8,7 @@ interface Servico {
 
 let catalogoServico: Servico[] = [];
 
+// Adicionar um serviço novo
 export function adicionarServico(servico: Servico) {
 
     if (servico.precoHora <= 0) {
@@ -32,4 +33,40 @@ export function adicionarServico(servico: Servico) {
         precoHora: servico.precoHora,
         totalServicos: catalogoServico.length
     };
+}
+
+// Listar todos os serviços
+
+export function listarServicos(): Servico[] {
+    // TODO: implimentar fetch de serviços
+    return catalogoServico
+
+}
+
+// apagar um serviço
+export function apagarServico(nome: string): boolean {
+    //TODO: implementar delete de serviço
+
+    const novoCatalogoTemp: Servico[] = []
+
+    for (let i = 0; i < catalogoServico.length; i++) {
+        if (catalogoServico[i]?.nome !== undefined && catalogoServico[i]?.nome !== nome) {
+            novoCatalogoTemp.push(catalogoServico[i]!)
+        }
+    } // Devolve um novo catalogo sem o servico que foi apagado
+
+    catalogoServico = novoCatalogoTemp
+
+    return true
+}
+
+//obter um servico pelo nome
+
+export function obterServico(nome: string): Servico | null {
+    for (let i = 0; i < catalogoServico.length; i++) {
+        if (catalogoServico[i]?.nome === nome) {
+            return catalogoServico[i]!
+        }
+    }
+    return null
 }
