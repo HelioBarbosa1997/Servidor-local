744072f4b3837516ef5f6f6e4d2c596a7082104e
Helio Barbosa <barbosahifb1997@gmail.com>
2026-05-07 20:35:39 -0100

    commit updated

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -94,7 +94,7 @@ export const userController = {
         if (!email || !password) {
             const response: ResponseType<null> = {
                 status: "error",
-                 message: "Credenciais inválidas",
+                message: "Credenciais inválidas",
                 data: null,
             }
             return res.status(400).json(response)
